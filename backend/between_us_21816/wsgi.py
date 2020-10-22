74c7a39c7c98578c7a1453031d07bdb8cfc3295b
Crowdbotics <team@crowdbotics.com>
2020-10-22 01:34:37 +0000

    Update models

--- backend/between_us_21816/wsgi.py
+++ backend/between_us_21816/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'between_us_21816.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "between_us_21816.settings")
 
 application = get_wsgi_application()
